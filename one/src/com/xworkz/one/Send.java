41d0c0277ffd3bc0512d202bf793496c0b44c9d9
Divyamc17 <divyamc17xworkz@gmail.com>
2022-11-23 04:04:42 -0800

    number

---
+++ one/src/com/xworkz/one/Send.java
@@ -0,0 +1,498 @@
+package com.xworkz.one;
+
+import org.springframework.context.annotation.Bean;
+
+public class Send {
+
+	@Bean
+	public Integer twentyOne() {
+		System.out.println("twentyOne");
+		return 21;
+
+	}
+	@Bean
+	public Integer twentytwo() {
+		System.out.println("twentytwo");
+		return 22;
+
+	}
+	@Bean
+	public Integer twentyThree() {
+		System.out.println("twentytThree");
+		return 23;
+
+	}
+	@Bean
+	public Integer twentyFour() {
+		System.out.println("twentyFour");
+		return 24;
+
+	}
+	@Bean
+	public Integer twentyFive() {
+		System.out.println("twentyFive");
+		return 25;
+
+	}
+	@Bean
+	public Integer twentySix() {
+		System.out.println("twentySix");
+		return 26;
+
+	}
+	@Bean
+	public Integer twentySeven () {
+		System.out.println("twentySeven");
+		return 27;
+
+	}
+	@Bean
+	public Integer twentyeight() {
+		System.out.println("twentyeight");
+		return 28;
+
+	}
+	@Bean
+	public Integer twentynine() {
+		System.out.println("twentynine");
+		return 29;
+
+	}
+	@Bean
+	public Integer thirty() {
+		System.out.println("thirty");
+		return 30;
+
+	}
+	@Bean
+	public Integer thirtyOne() {
+		System.out.println("thirtyOne");
+		return 31;
+
+	}
+	@Bean
+	public Integer thirtyTwo() {
+		System.out.println("thirtyTwo");
+		return 32;
+
+	}
+
+	@Bean
+	public Integer thirtyThree() {
+		System.out.println("thirtyThree");
+		return 33;
+
+	}
+	@Bean
+	public Integer thirtyFour() {
+		System.out.println("thirtyFour");
+		return 34;
+
+	}
+	@Bean
+	public Integer thirtyFive() {
+		System.out.println("thirtyFive");
+		return 35;
+
+	}
+	@Bean
+	public Integer thirtySix() {
+		System.out.println("thirtySix");
+		return 36;
+
+	}
+	@Bean
+	public Integer thirtySeven() {
+		System.out.println("thirtySeven");
+		return 37;
+
+	}
+	@Bean
+	public Integer thirtyEight() {
+		System.out.println("thirtyEight");
+		return 38;
+
+	}
+	@Bean
+	public Integer thirtyNine() {
+		System.out.println("thirtyNine");
+		return 39;
+
+	}
+	@Bean
+	public Integer fourty() {
+		System.out.println("Fourty");
+		return 40;
+
+	}
+	@Bean
+	public Integer fourtyOne() {
+		System.out.println("FourtyOne");
+		return 41;
+
+	}
+	@Bean
+	public Integer fourtyTwo() {
+		System.out.println("FourtyTwo");
+		return 42;
+
+	}
+	@Bean
+	public Integer fourtyThree() {
+		System.out.println("FourtyThree");
+		return 43;
+
+	}
+
+	@Bean
+	public Integer fourtyFour() {
+		System.out.println("FourtyFour");
+		return 44;
+
+	}
+	@Bean
+	public Integer fourtyFive() {
+		System.out.println("FourtyFive");
+		return 45;
+
+	}
+	@Bean
+	public Integer fourtySix() {
+		System.out.println("FourtySix");
+		return 46;
+
+	}
+	@Bean
+	public Integer fourtySeven() {
+		System.out.println("FourtySeven");
+		return 47;
+
+	}
+	@Bean
+	public Integer fourtyEight() {
+		System.out.println("FourtyEight");
+		return 48;
+
+	}
+	@Bean
+	public Integer fourtyNine() {
+		System.out.println("FourtyNine");
+		return 49;
+
+	}
+
+	@Bean
+	public Integer fifty() {
+		System.out.println("Fifty");
+		return 50;
+
+	}
+	@Bean
+	public Integer fiftyOne() {
+		System.out.println("FiftyOne");
+		return 51;
+
+	}
+	@Bean
+	public Integer fiftyTwo() {
+		System.out.println("FiftyTwo");
+		return 52;
+
+	}
+	@Bean
+	public Integer fiftyThree() {
+		System.out.println("FiftyThree");
+		return 53;
+
+	}
+	@Bean
+	public Integer fiftyFour() {
+		System.out.println("FiftyFour");
+		return 54;
+
+	}
+	@Bean
+	public Integer fiftyFive() {
+		System.out.println("FiftyFive");
+		return 55;
+
+	}
+	@Bean
+	public Integer fiftySix() {
+		System.out.println("FiftySix");
+		return 56;
+
+	}
+	@Bean
+	public Integer fiftySeven() {
+		System.out.println("FiftySeven");
+		return 57;
+
+	}
+	@Bean
+	public Integer fiftyEight() {
+		System.out.println("FiftyEight");
+		return 58;
+
+	}
+	@Bean
+	public Integer fiftyNine() {
+		System.out.println("FiftyNine");
+		return 59;
+
+	}
+	@Bean
+	public Integer sixtey() {
+		System.out.println("Sixtey");
+		return 60;
+
+	}
+	@Bean
+	public Integer sixteyOne() {
+		System.out.println("SixteyOne");
+		return 61;
+
+	}
+	@Bean
+	public Integer sixtyTwo() {
+		System.out.println("SixtyTwo");
+		return 62;
+
+	}
+	@Bean
+	public Integer sixtyThree() {
+		System.out.println("SixtyThree");
+		return 63;
+
+	}
+	@Bean
+	public Integer sixtyFour() {
+		System.out.println("SixtyFour");
+		return 64;
+
+	}
+	@Bean
+	public Integer sixtyFive() {
+		System.out.println("SixtyFive");
+		return 65;
+
+	}
+	@Bean
+	public Integer sixtySix() {
+		System.out.println("SixtySix");
+		return 66;
+
+	}
+	@Bean
+	public Integer sixtySeven() {
+		System.out.println("SixtySeven");
+		return 67;
+
+	}
+	@Bean
+	public Integer sixtyEight() {
+		System.out.println("SixtyEight");
+		return 68;
+
+	}
+	@Bean
+	public Integer sixtyNine() {
+		System.out.println("SixtyNine");
+		return 69;
+
+	}
+	@Bean
+	public Integer seventy() {
+		System.out.println("Seventy");
+		return 70;
+
+	}
+	@Bean
+	public Integer seventyOne() {
+		System.out.println("SeventyOne");
+		return 71;
+
+	}
+	@Bean
+	public Integer seventyTwo() {
+		System.out.println("SeventyTwo");
+		return 72;
+
+	}
+	@Bean
+	public Integer seventyThree() {
+		System.out.println("SeventyThree");
+		return 73;
+
+	}
+	@Bean
+	public Integer seventyFour() {
+		System.out.println("SeventyFour");
+		return 74;
+
+	}
+	@Bean
+	public Integer seventyFive() {
+		System.out.println("SeventyFive");
+		return 75;
+
+	}
+	@Bean
+	public Integer seventySix() {
+		System.out.println("SeventySix");
+		return 76;
+
+	}
+	@Bean
+	public Integer seventySeven() {
+		System.out.println("SeventySeven");
+		return 77;
+
+	}
+	@Bean
+	public Integer seventyEight() {
+	System.out.println("seventyEight");
+		return 78;
+
+	}
+	 @Bean
+	public Integer seventyNine() {
+		System.out.println("seventyNine");
+		return 78;
+
+	}
+	@Bean
+	public Integer eighty() {
+		System.out.println("eighty");
+		return 80;
+
+	}
+	@Bean
+	public Integer eightyOne() {
+		System.out.println("eightyOne");
+		return 81;
+
+	}
+	 @Bean
+	public Integer eightyTwo() {
+		System.out.println("eightyTwo");
+		return 82;
+
+	}
+	@Bean
+	public Integer eightyThree() {
+		System.out.println("eightyThree");
+		return 83;
+
+	}
+	@Bean
+	public Integer eightyFour() {
+		System.out.println("eightyFour");
+		return 84;
+
+	}
+	@Bean
+	public Integer eightyFive() {
+		System.out.println("eightyFive");
+		return 85;
+
+	}
+	@Bean
+	public Integer eightySix() {
+		System.out.println("eightySix");
+		return 86;
+
+	}
+	@Bean
+	public Integer eightySeven() {
+		System.out.println("eightySeven");
+		return 87;
+
+	}
+	@Bean
+	public Integer eightyEight() {
+		System.out.println("eightyEight");
+		return 88;
+
+	}
+	@Bean
+	public Integer eightyNine() {
+		System.out.println("eightyNine");
+		return 89;
+
+	}
+
+	@Bean
+	public Integer ninty() {
+		System.out.println("ninty");
+		return 90;
+
+	}
+	@Bean
+	public Integer nintyOne() {
+		System.out.println("nintyOne");
+		return 91;
+
+	}
+	@Bean
+	public Integer nintyTwo() {
+		System.out.println("NintyTwo");
+		return 92;
+
+	}
+	@Bean
+	public Integer nintyThree() {
+		System.out.println("nintyThree");
+		return 93;
+
+	} 
+	@Bean
+	public Integer nintyFour() {
+		System.out.println("nintyFour");
+		return 94;
+
+	}
+	@Bean
+	public Integer nintyFive() {
+		System.out.println("nintyFive");
+		return 95;
+
+	}
+	@Bean
+	public Integer nintySix() {
+		System.out.println("nintySix");
+		return 96;
+
+	}
+	@Bean
+	public Integer nintySeven() {
+		System.out.println("nintySeven");
+		return 97;
+
+	}
+	@Bean
+	public Integer nintyEight() {
+		System.out.println("nintyEight");
+		return 98;
+
+	}
+	@Bean
+	public Integer nintyNine() {
+		System.out.println("nintyNine");
+		return 99;
+
+	}
+	@Bean
+	public Integer hundred() {
+		System.out.println("hundred");
+		return 100;
+
+	}
+
+	 
+	 
+
+
+	
+	
+}
